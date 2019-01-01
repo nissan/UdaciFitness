9a8780fc286ea36a028c46c42854c7ff71a3ebee
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-02 02:49:52 +1000

    add steppers and slider components to UI

---
+++ components/UdaciSlider.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export const UdaciSlider = props => {
+  return (
+    <View>
+      <Text>UdaciSlider</Text>
+    </View>
+  );
+};
+
+export default UdaciSlider;
---
+++ components/UdaciSteppers.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export const UdaciSteppers = props => {
+  return (
+    <View>
+      <Text>UdaciSteppers</Text>
+    </View>
+  );
+};
+
+export default UdaciSteppers;
