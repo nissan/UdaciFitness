5f84229eb01262bdd81c1aa612cd257eeb652a89
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-02 02:11:14 +1000

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+
+export default class App extends React.Component {
+  componentDidMount(){
+    // console.log("Before");
+    // debugger;
+    // console.log("After");
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up the App.js file to start working on your app!</Text>
+        <Ionicons name="ios-pizza" color="red" size={100}  />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
