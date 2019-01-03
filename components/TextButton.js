cdc813fd779bbd946352f00c14c20e1fe82bff5f
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-03 20:32:55 +1000

    style date header, slider, reset button

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { purple } from "../utils/colors";
 
-export const TextButton = ({ children, onPress }) => {
+export const TextButton = ({ children, onPress, style = {} }) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text>{children}</Text>
+      <Text style={[styles.reset, style]}>{children}</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: "center",
+    color: purple
+  }
+});
 
 export default TextButton;
